66e5206ea2e889a8940e8d4ec66b4c36a142de1b
Plamen Yankov <p.s.yankov@abv.bg>
2014-08-30 13:43:33 +0300

    adding excel

--- app/views/partials/navbar.blade.php
+++ app/views/partials/navbar.blade.php
@@ -9,10 +9,12 @@
             <li><a href="http://localhost:8000/goals/create">Add Goal</a></li>
         </ul> <ul class="left">
             <li><a href="http://localhost:8000/list">List</a></li>
-        </ul> <ul class="left">
+        </ul>
+        <ul class="left">
             <li><a href="http://localhost:8000/list/create">Add List</a></li>
-        </ul></ul> <ul class="left">
-            <li><a href="http://localhost:8000/list/create">Add List</a></li>
+        </ul>
+        <ul class="left">
+            <li><a href="http://localhost:8000/list/create">Excel</a></li>
         </ul>
 
         </section>
